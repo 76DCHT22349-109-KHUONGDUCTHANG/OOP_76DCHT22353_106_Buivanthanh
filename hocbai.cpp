32486bce276c9f264e88ad5220f73463e326f4cd
76DCHT22349_109_KHƯƠNGĐỨCTHẮNG <khuongthang30102007@gmail.com>
2026-08-22 10:44:24 +0700

    Update hocbai.cpp

--- hocbai.cpp
+++ hocbai.cpp
@@ -5,11 +5,7 @@ using namespace std;
 
 class nhanvien {
 private:
-    string manv;
     string hovaten;
-    string diachi;
-    double luong;
-    double heso;
 public:
     void nhap() {
         cout << "Nhap ho va ten: ";
@@ -26,10 +22,10 @@ int main() {
     int n;
     cout << "Nhap so luong nhan vien: ";
     cin >> n;
-    cin.ignore();
+    cin.ignore(); // bỏ ký tự '\n' sau khi nhập số
 
     // ===== VECTOR =====
-    cout << "\n=== CACH 1: VECTOR ===\n";
+    cout << "\n CACH 1: VECTOR\n";
     vector<nv> ds(n);
     for (int i = 0; i < n; i++) {
         ds[i].nhap();
@@ -39,7 +35,7 @@ int main() {
     }
 
     // ===== MẢNG ĐỘNG =====
-    cout << "\n=== CACH 2: MANG DONG ===\n";
+    cout << "\n CACH 2: MANG DONG\n";
     nv *dsnv2 = new nv[n];
     for (int i = 0; i < n; i++) {
         dsnv2[i].nhap();
@@ -50,8 +46,8 @@ int main() {
     delete[] dsnv2;
 
     // ===== MẢNG TĨNH =====
-    cout << "\n=== CACH 3: MANG TINH ===\n";
-    nv dsnv3[100]; // cố định 100 phần tử, nhập tối đa n
+    cout << "\nCACH 3: MANG TINH \n";
+    nv dsnv3[100]; 
     for (int i = 0; i < n; i++) {
         dsnv3[i].nhap();
     }
